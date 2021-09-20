3e0df6b6b18ba325fb31362e0e6ea3229c0b1007
Charlton Omondi <omondioware@gmail.com>
2021-09-20 06:51:05 +0300

    add JS functionality for checking answers

---
+++ app.js
@@ -0,0 +1,20 @@
+//create array to store all the correct answers of the quiz
+const correctAnswers = ['B','B','B', 'B'];
+const form =document.querySelector('.quiz-form');
+
+form.addEventListener('submit', e => {
+    e.preventDefault();
+    //create score for user based on their answer
+    let score = 0;
+    //get user answers from each input field
+    const userAnswers = [form.q1.value, form.q2.value, form.q3.value, form.q4.value];
+
+    //compare user inputs to the correct answers
+    userAnswers.forEach((answer, index) => {
+        if(answer === correctAnswers[index]){
+           score += 25; 
+        }
+    });
+
+    console.log("SCORE:", score)
+});
